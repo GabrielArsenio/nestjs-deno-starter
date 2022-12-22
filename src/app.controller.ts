a01d92cd8cb18ca4f66310d8056c23c3e72188d5
GabrielArsenio <gabrielarsenioo@gmail.com>
2022-12-22 20:33:55 -0300

    Add AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Inject } from "@nestjs/common";
+import { AppService } from "./app.service.ts";
 
 @Controller()
 export class AppController {
+  constructor(
+    @Inject(AppService) private service: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return 'Hello World!';
+    return this.service.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller.ts";
+import { AppService } from "./app.service.ts";
 
 @Module({
   imports: [],
+  providers: [AppService],
   controllers: [AppController],
 })
 export class AppModule {}
